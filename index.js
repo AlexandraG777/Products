6168fa52f89f856b8fece80baefc93d6f1e1d5fd
AlexandraG777 <117565044+AlexandraG777@users.noreply.github.com>
2025-03-04 20:55:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let user = {
+    man1: {
+        name: "Fgh",
+        age: 25,
+        citi: 'NN '+'Svobodi'
+    },
+    man2: {
+        name: "Kjuy",
+        age: 28,
+        man3: {
+            name: "Pipa",
+            age: 32,
+            citi: 'NN '+'Svobodi'
+        }
+    },
+
+};
+console.log(user);
